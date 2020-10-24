e1c6cdf3afb2767e968b59ff35d6768c7c8aec2c
Iwan Skworcow <drina@ox-f3.msk.21-school.ru>
2020-10-24 20:29:18 +0300

    next_ftps

--- start_services.sh
+++ start_services.sh
@@ -10,12 +10,16 @@ docker build -t php php/
 docker build -t mariadb mysql/
 docker build -t wordpress wordpress/
 docker build -t influxdb influxdb/
+docker build -t telegraf telegraf/
+docker build -t grafana grafana/
 
 kubectl apply -f mysql/mariadb.yaml
 kubectl apply -f nginx/nginx.yaml
 kubectl apply -f php/php.yaml
 kubectl apply -f wordpress/wordpress.yaml
 kubectl apply -f  influxdb/influxdb.yaml
+kubectl apply -f telegraf/telegraf.yaml
+kubectl apply -f grafana/grafana.yaml
 
 minikube dashboard
 
